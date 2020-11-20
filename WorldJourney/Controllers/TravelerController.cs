645c14983445de3aa19b2cfa88785125e4c0b2a2
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-11-20 21:12:53 +0100

    Add project files.

---
+++ WorldJourney/Controllers/TravelerController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldJourney.Controllers
+{
+    public class TravelerController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index(string name)
+        {
+            ViewBag.VisiterName = name;
+            return View();
+        }
+    }
+}
